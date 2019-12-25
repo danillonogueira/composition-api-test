3dbe11f4e34280cb21d1967631c1a1a1b7d9ae56
Danillo Nogueira <dnll_nog@yahoo.com>
2019-12-25 17:15:32 -0300

    sketching responsiveness with bootstrap

---
+++ src/views/Home.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1 class="text-info">Fill out a note to have the time of your liiiiiife! :)</h1>
+      </div>
+    </div>
+    <div class="row my-4">
+      <div class="col-lg-12">
+        <form>
+          <div class="form-group">
+            <label>Title</label>
+            <input type="text" class="form-control" />
+            <small class="form-text text-muted">Every good note gotta have a big nice title, aye?</small>
+          </div>
+          <div class="form-group">
+            <label>Note</label>
+            <textarea class="form-control" rows="4"></textarea>
+            <small class="form-text text-muted">Every good note gotta have the note itself, ammmk?!</small>
+          </div>
+          <button class="btn btn-primary">JOT DOWN!</button>
+        </form>
+      </div>
+    </div>
+    <div class="row my-4 d-flex justify-content-center">
+      <div class="col-lg-4">
+        <div class="card my-2" style="width: 100%;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card my-2" style="width: 100%;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card my-2" style="width: 100%;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card my-2" style="width: 100%;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card my-2" style="width: 100%;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-4">
+        <div class="card my-2" style="width: 100%;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Another link</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    setup() {
+
+    } 
+  }
+</script>
