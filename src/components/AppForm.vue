ccf296feeb064cc9bf901e08950667c102b461b9
Danillo Nogueira <dnll_nog@yahoo.com>
2020-02-12 21:08:52 -0300

    components added

---
+++ src/components/AppForm.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid p-0">
+    <div class="row">
+      <div class="col-lg-12">
+        <form>
+          <div class="form-group">
+            <label>Title</label>
+            <input type="text" class="form-control" />
+            <small class="form-text text-muted">Every good note gotta have a big nice title, aye?</small>
+          </div>
+          <div class="form-group">
+            <label>Note</label>
+            <textarea class="form-control" rows="4"></textarea>
+            <small class="form-text text-muted">Every good note gotta have the note itself, ammmk?!</small>
+          </div>
+          <button @click.prevent="" class="btn btn-primary">JOT DOWN!</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'app-form'
+  }
+</script>
+
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">
+    <div class="col-12 text-center">
+      <h1 class="text-info">Fill out a note to have the time of your liiiiiife! :)</h1>
+    </div>
+  </div> 
+</template>
+
+<script>
+  export default {
+    name: 'app-header'
+  }
+</script>
